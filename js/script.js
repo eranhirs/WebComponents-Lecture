c089e34be782591ac9dc0cd38358ada4fa86c35c
hirscheran <hirscheran@gmail.com>
2014-09-26 21:09:44 +0300

    Actually added web components in slide

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-Reveal.addEventListener('slidechanged', function(event) {
-	// Only in kitty slide
-	if (event.currentSlide.id == "importKittyHtml") {
-	
-		// Grab image and set onclick
-		var img = document.querySelector('#kittyImage');
-		img.onclick = function() { console.log("Hello world!"); };
-		
-	}
-} );
-
-
 Utilities = (function() {
 
 	// Add slide numbers
@@ -22,11 +10,52 @@ Utilities = (function() {
 		var yyyy = today.getFullYear();
 		if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = dd+'/'+mm+'/'+yyyy;
 		return today;
-	}
-	
-	
+	};
+
+    // Take the slides from html imports and append them to DOM
+    var initialize = function() {
+        // Get all slides from imports
+        var slidesContainer = document.querySelector('.slides');
+        var links = document.head.querySelectorAll('link[rel="import"]');
+
+        for (i=0; i < links.length; i++) {
+            var link = links[i];
+            var slide = link.import.querySelector('.slide');
+            slidesContainer.appendChild(slide.cloneNode(true));
+        }
+
+        // Add Reveal listener
+        Reveal.addEventListener('slidechanged', function(event) {
+            // Only in kitty slide
+            if (event.currentSlide.id == "importKittyHtml") {
+
+                // Grab image and set onclick
+                var img = document.querySelector('#kittyImage');
+                img.onclick = function() { console.log("Hello world!"); };
+
+            }
+        } );
+    };
+
+    var getKittyImageFromImport = function() {
+        var importDoc = document.currentScript.ownerDocument;
+        var mainDoc = document;
+
+        // Grab content
+        var link = importDoc.querySelector('link[rel="import"]');
+        var content = link.import;
+
+        // Grab relevant DOM from kitty page
+        var kitty = content.querySelector('.kitty');
+
+        // Append to our DOM
+        var examplePlaceHolder = importDoc.querySelector('#examplePlaceHolder');
+        examplePlaceHolder.appendChild(kitty.cloneNode(true));
+    };
+
 	return {
-		'getCurrentDate': getCurrentDate
+		'getCurrentDate': getCurrentDate,
+        'initialize': initialize,
+        'getKittyImageFromImport': getKittyImageFromImport
 	};
 }());
-
